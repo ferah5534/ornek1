9f9e223245e5bea72114c230ab61d6d9dd433068
ferah5534 <hakferah@gmail.com>
2020-11-24 13:45:21 -0700

    version 2

--- src/test1.java
+++ src/test1.java
@@ -4,7 +4,9 @@ public class test1 {
         System.out.println(".gitignore dosyasina hangi dosyalar gitmeyecekse onlar yaziliyor");
     }
 
-    private <Sena> void Sena(Sena sena){
+   // public <Sena> void Sena(Sena sena){
         System.out.println("Sena");
+
+        System.out.println();
     }
 }
